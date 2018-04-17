327ece845636ac5109f79b63bb86474c26e89e66
Karen Chen <kchen6600@gmail.com>
2018-04-17 13:54:18 -0400

    graphs

--- static/graphs.js
+++ static/graphs.js
@@ -36,33 +36,201 @@
     d3.select(self.frameElement).style("height", "600px"); 
 
 
+var svg = d3.select("body")
+	.append("svg")
+	.append("g")
+
+svg.append("g")
+	.attr("class", "slices");
+svg.append("g")
+	.attr("class", "labels");
+svg.append("g")
+	.attr("class", "lines");
+
+var width = 960,
+    height = 450,
+	radius = Math.min(width, height) / 2;
+
+var pie = d3.layout.pie()
+	.sort(null)
+	.value(function(d) {
+		return d.count;
+	});
+
+var arc = d3.svg.arc()
+	.outerRadius(radius * 0.8)
+	.innerRadius(radius * 0.4);
+
+var outerArc = d3.svg.arc()
+	.innerRadius(radius * 0.9)
+	.outerRadius(radius * 0.9);
+
+svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+//var data1 = [{'count': 31, 'label': 'Male'}, {'count': 14, 'label': 'Female'}]
+var color = d3.scale.category20();
+
+var natdatagender = [{'count': 1804, 'label':'Male'}, {'count': 747, 'label':'Female'}];
+var natdatarace = [{'count': 28, 'label':'Black'}, {'count': 2466, 'label':'Asian'},{'count': 18, 'label':'White'}, {'count': 39, 'label':'Hispanic'}];
+var natdataage = [{'count': 400, 'label':'0-20'}, {'count': 409, 'label':'21-30'}, {'count': 221, 'label':'31-40'}, {'count': 98, 'label':'41-50'}, {'count':114, 'label':'51+'}];
 
 
-var data = [
-  [11975,  5871, 8916, 2868],
-  [ 1951, 10048, 2060, 6171],
-  [ 8010, 16145, 8090, 8045],
-];
+var key = function(d){ return d.data.label; };
+var val = function(d){return d.data.count; };
 
-//  margin, radius, and color scale
-var m = 10,
-    r = 100,
-    z = d3.scale.category20c();
+var color = d3.scale.category20();
 	
-	
-var svg = d3.select("body").selectAll("svg")
-    .data(data)
-  .enter().append("svg")
-    .attr("width", (r+m)*2)
-    .attr("height", (r + m) * 2)
-  .append("g")
-    .attr("transform", "translate(" + (r+m) + "," + (r+m+5) + ")");
+function mergeWithFirstEqualZero(first, second){
+	var secondSet = d3.set(); second.forEach(function(d) { secondSet.add(d.label); });
 
-svg.selectAll("path")
-    .data(d3.layout.pie())
-  .enter().append("path")
-    .attr("d", d3.svg.arc()
-        .innerRadius(r / 2)
-        .outerRadius(r))
-    .style("fill", function(d, i) { return z(i); });
+	var onlyFirst = first
+		.filter(function(d){ return !secondSet.has(d.label) })
+		.map(function(d) { return {label: d.label, value: 0}; });
+	return d3.merge([ second, onlyFirst ])
+		.sort(function(a,b) {
+			return d3.ascending(a.label, b.label);
+		});
+}
+
+var duration = 500;
+
+change(natdatarace);
+function change(data) {
+	//var duration = +document.getElementById("duration").value;
+	var data0 = svg.select(".slices").selectAll("path.slice")
+		.data().map(function(d) { return val });
+	if (data0.length == 0) data0 = data;
+	var was = mergeWithFirstEqualZero(data, data0);
+	var is = mergeWithFirstEqualZero(data0, data);
+	console.log(pie(was));
+	console.log(is);
+	//console.log(data1[1].count);
+	/* ------- SLICE ARCS -------*/
+
+	var slice = svg.select(".slices").selectAll("path.slice")
+		.data(pie(was), key);
+
+	slice.enter()
+		.insert("path")
+		.attr("class", "slice")
+		.style("fill", function(d,i) { return color(i);})//return d.data.label; })
+		.each(function(d) {
+			this._current = d;
+		});
+
+	slice = svg.select(".slices").selectAll("path.slice")
+		.data(pie(is), key);
+
+	slice		
+		.transition().duration(duration)
+		.attrTween("d", function(d) {
+			var interpolate = d3.interpolate(this._current, d);
+			var _this = this;
+			return function(t) {
+				_this._current = interpolate(t);
+				return arc(_this._current);
+			};
+		});
+
+	slice = svg.select(".slices").selectAll("path.slice")
+		.data(pie(data), key);
+
+	slice
+		.exit().transition().delay(duration).duration(0)
+		.remove();
+
+	/* ------- TEXT LABELS -------*/
+
+	var text = svg.select(".labels").selectAll("text")
+		.data(pie(was), key);
+
+	text.enter()
+		.append("text")
+		.attr("dy", ".35em")
+		.style("opacity", 0)
+		.text(function(d) {
+			return d.data.label;
+		})
+		.each(function(d) {
+			this._current = d;
+		});
 	
+	function midAngle(d){
+		return d.startAngle + (d.endAngle - d.startAngle)/2;
+	}
+
+	text = svg.select(".labels").selectAll("text")
+		.data(pie(is), key);
+
+	text.transition().duration(duration)
+		.style("opacity", function(d) {
+			return d.data.value == 0 ? 0 : 1;
+		})
+		.attrTween("transform", function(d) {
+			var interpolate = d3.interpolate(this._current, d);
+			var _this = this;
+			return function(t) {
+				var d2 = interpolate(t);
+				_this._current = d2;
+				var pos = outerArc.centroid(d2);
+				pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
+				return "translate("+ pos +")";
+			};
+		})
+		.styleTween("text-anchor", function(d){
+			var interpolate = d3.interpolate(this._current, d);
+			return function(t) {
+				var d2 = interpolate(t);
+				return midAngle(d2) < Math.PI ? "start":"end";
+			};
+		});
+	
+	text = svg.select(".labels").selectAll("text")
+		.data(pie(data), key);
+
+	text
+		.exit().transition().delay(duration)
+		.remove();
+
+	/* ------- SLICE TO TEXT POLYLINES -------*/
+
+	var polyline = svg.select(".lines").selectAll("polyline")
+		.data(pie(was), key);
+	
+	polyline.enter()
+		.append("polyline")
+		.style("opacity", 0)
+		.each(function(d) {
+			this._current = d;
+		});
+
+	polyline = svg.select(".lines").selectAll("polyline")
+		.data(pie(is), key);
+	
+	polyline.transition().duration(duration)
+		.style("opacity", function(d) {
+			return d.data.value == 0 ? 0 : .5;
+		})
+		.attrTween("points", function(d){
+			this._current = this._current;
+			var interpolate = d3.interpolate(this._current, d);
+			var _this = this;
+			return function(t) {
+				var d2 = interpolate(t);
+				_this._current = d2;
+				var pos = outerArc.centroid(d2);
+				pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+				return [arc.centroid(d2), outerArc.centroid(d2), pos];
+			};			
+		});
+	
+	polyline = svg.select(".lines").selectAll("polyline")
+		.data(pie(data), key);
+	
+	polyline
+		.exit().transition().delay(duration)
+		.remove();
+};
+
+
+
